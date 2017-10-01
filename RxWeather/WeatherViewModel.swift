d5f046eadcb2b7127000f20c067eddce8a7f756b
enniobovyn <enniobovyn@gmail.com>
2017-10-01 23:40:50 +0200

    Code refactoring & other improvements

--- RxWeather/WeatherViewModel.swift
+++ RxWeather/WeatherViewModel.swift
@@ -1,90 +1,78 @@
-//
-//  WeatherViewModel.swift
-//  RxWeather
-//
-//  Created by Ennio Bovyn on 5/02/16.
-//  Copyright © 2016 Ennio Bovyn. All rights reserved.
-//
-
-import RxCocoa
 import RxSwift
-
+import RxCocoa
+import RxAlamofire
+import Alamofire
 import SwiftyJSON
 
-import Alamofire
-import RxAlamofire
-
 class WeatherViewModel {
-    
+
     private struct Constants {
         static let URLPrefix = "http://api.openweathermap.org/data/2.5/weather?q="
         static let URLSuffix = "&units=metric&appid=7b698c5541eae81b147d972c3dbc8b6a"
     }
-    
+
+    let cityName = PublishSubject<String>()
+    let temp = PublishSubject<String>()
+    let errorAlertController = PublishSubject<UIAlertController>()
+
     private let disposeBag = DisposeBag()
-    
-    var errorAlertController = PublishSubject<UIAlertController>()
-    
-    var cityName = PublishSubject<String>()
-    var temp = PublishSubject<String>()
-    
+
     var searchText:String? {
         didSet {
             if let text = searchText {
-                if (text.characters.count >= 3) {
-                    let urlString = Constants.URLPrefix + text.replacingOccurrences(of: " ", with: "%20") + Constants.URLSuffix
-                  getWeatherForRequest(urlString: urlString)
+                if text.characters.count >= 3 {
+                    if let encodedText = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+                        let urlString = Constants.URLPrefix + encodedText + Constants.URLSuffix
+                        getWeather(for: urlString)
+                    } else {
+                        print("Something went wrong with encoding the search text.")
+                    }
                 }
             }
         }
     }
-    
+
     private var weather:Weather? {
         didSet {
             updateModel()
         }
     }
-    
+
     private func updateModel() {
-        
         if let city = weather?.cityName {
-        
             self.cityName.on(.next(city))
-        
+
             if let temp = weather?.temp {
                 self.temp.on(.next("\(temp)°C"))
             }
-            
         }
-        
     }
     
-    private func getWeatherForRequest(urlString: String) {
+    private func getWeather(for urlString: String) {
         Alamofire.request(urlString).rx.responseJSON()
             .observeOn(MainScheduler.instance)
             .subscribe(
                 onNext: { dataResponse in
-                  if let data = dataResponse.data {
-                    let json = JSON(data)
-                    if let error = json["message"].string {
-                      self.postError(title: "Error", message: error)
-                      return
+                    if let data = dataResponse.data {
+                        let json = JSON(data)
+                        if let error = json["message"].string {
+                            self.postError(title: "Error", message: error)
+                            return
+                        }
+                        self.weather = Weather(json: json)
                     }
-                    self.weather = Weather(json: json)
-                  }
                 },
                 onError: { error in
                     print("Got error")
                     let gotError = error as NSError
-                    
                     print(gotError.domain)
-                  self.postError(title: "\(gotError.code)", message: gotError.domain)
-            })
+                    self.postError(title: "\(gotError.code)", message: gotError.domain)
+                })
             .addDisposableTo(disposeBag)
     }
-    
+
     private func postError(title: String, message: String) {
-      errorAlertController.on(.next(UIAlertController(title: title, message: message, preferredStyle: .alert)))
+        errorAlertController.on(.next(UIAlertController(title: title, message: message, preferredStyle: .alert)))
     }
 
 }
